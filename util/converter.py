c215ccd8f27c6f0055ff0f1602689fcb1ae05952
LOPERADRI <111729976+loperadri@users.noreply.github.com>
2026-06-16 18:52:07 +0200

    Implement converters for Cashier, Customer, and Product

--- util/converter.py
+++ util/converter.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-#Write your code here
+from users.user import Cashier, Customer
+from products.product import Hamburger, Soda, Drink, HappyMeal
 
 class Converter(ABC):
   @abstractmethod
@@ -11,13 +12,32 @@ class Converter(ABC):
 
 class CashierConverter(Converter):
   def convert(self,dataFrame):    
-    #Write your code here
+    cashiers = []
+    for index, row in dataFrame.iterrows():
+      cashier = Cashier(row['dni'], row['name'], row['age'], row['timetable'], row['salary'])
+      cashiers.append(cashier)
+    return cashiers
     pass
 
 class CustomerConverter(Converter):
-  #Write your code here
-  pass
+  def convert(self,dataFrame):
+    customers = []
+    for index, row in dataFrame.iterrows():
+      customer = Customer(row['dni'], row['name'], row['age'], row['email'], row['postalcode'])
+      customers.append(customer)
+    return customers
+    pass
 
 class ProductConverter(Converter):
-  #Write your code here
-  pass
+  def convert(self, df_hamburgers, df_sodas, df_drinks, df_happymeals):
+    products = []
+    for index, row in df_hamburgers.iterrows():
+      products.append(Hamburger(row['id'], row['name'], row['price']))
+    for index, row in df_sodas.iterrows():
+      products.append(Soda(row['id'], row['name'], row['price']))
+    for index, row in df_drinks.iterrows():
+      products.append(Drink(row['id'], row['name'], row['price']))
+    for index, row in df_happymeals.iterrows():
+      products.append(HappyMeal(row['id'], row['name'], row['price']))
+    return products
+    pass
